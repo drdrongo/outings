7a811ac3c09d70f6760de229813d7c390c4253ae
drdrongo <hayatoclarke@gmail.com>
2023-04-16 23:49:34 +0900

    fonts for mui in pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
         <meta name='theme-color' content='#FFC0CB' />
       </Head>
 
